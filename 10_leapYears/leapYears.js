8359ca70c21bb2061673c31d5b98378ca2cdc589
Justin <justincrypto702@gmail.com>
2025-11-19 13:54:24 -0800

    Finish javascript ex 10 leapYears

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
+const byFour = year % 4 === 0;
+const byHundred = year % 100 === 0;
+const byFourHundred = year % 400 === 0;
 
+if (byFour && (!byHundred || byFourHundred)){
+  return "true";
+} else {
+    return "false";
+}
 };
-
+console.log(leapYears(2000));
+//leapYears(2000) // is a leap year: returns true
+// leapYears(1985) // is not a leap year: returns false
 // Do not edit below this line
 module.exports = leapYears;
