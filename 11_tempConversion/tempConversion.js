3fb3357ce22c83b53316f9cddc6a85b4e1589719
Justin <justincrypto702@gmail.com>
2025-11-19 14:21:06 -0800

    Finish javascript exercise 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+ const convertF = (num - 32)/1.8;
+  return Math.round((convertF + Number.EPSILON) * 100) /100;
 };
+console.log(convertToCelsius(110));
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+ const covertC = (num * 1.8) +32;
+  return Math.round((convertC + Number.EPSILON) * 100) /100;
 };
+console.log(convertToCelsius(18));
 
+//C = F - 32) / 1.8
+//F = (C*1.8) +32
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
